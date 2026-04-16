4fb1b0f0d28fe2748c29bd1682f6f095cd5030a9
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2026-04-16 14:09:14 +0400

    ssasas

--- src/app/[locale]/glossary-faqs/page.jsx
+++ src/app/[locale]/glossary-faqs/page.jsx
@@ -2,40 +2,44 @@ import { getDictionary } from "@/i18n/request";
 import InnerPageBanner from "../components/common/InnerPageBanner";
 import GlossaryList from "./components/glossaryList";
 import { getPageMetadata } from "@/lib/metadata/getPageMetadata";
-import { translationTextByPath } from "@/i18n/tranlsationText";
+
 export async function generateMetadata({ params }) {
-  const { locale } = await params;
+  const { locale } = params;
   const dict = await getDictionary(locale);
+
   return getPageMetadata({
     locale,
     key: "glossaryFaqs",
     dict,
     path: "glossary-faqs",
-    fallbackTitle: "Glossary and FAQs - GTC FX",
-    fallbackDescription: "Explore key trading terms and find answers to common questions about GTCFX’s products, services, and trading conditions.",
+    fallbackTitle: "Trading Glossary & FAQs | GTCFX",
+    fallbackDescription:
+      "Explore key trading terms and FAQs to better understand financial markets and improve your trading knowledge.",
   });
 }
+
 export default async function GlossaryFaqsPage({ params }) {
-  const { locale } = await params;
+  const { locale } = params;
   const dict = await getDictionary(locale);
- const glossaryPage = dict?.glossaryPage || {};
+  const glossaryPage = dict?.glossaryPage || {};
 
   return (
     <>
-     <InnerPageBanner
+      <InnerPageBanner
         title={glossaryPage?.bannerTitle}
         description={glossaryPage?.bannerDescription}
         backgroundImage="/breadcamp/faqs.webp"
         mobileBackgroundImage="/breadcamp/faq-mobile.webp"
       />
-         <section className="relative overflow-hidden bg-gradient-to-b from-[#F8FAFC] via-[#fff] to-[#fff] py-10 md:py-16">
-          <div className="container mx-auto">
-            <GlossaryList
-              browseByLetter={glossaryPage?.browseByLetter}
-              noTerms={glossaryPage?.noTerms}
-            />
-          </div>
-        </section>
-     </>
+
+      <section className="relative overflow-hidden bg-gradient-to-b from-[#F8FAFC] via-white to-white py-10 md:py-16">
+        <div className="container">
+          <GlossaryList
+            browseByLetter={glossaryPage?.browseByLetter}
+            noTerms={glossaryPage?.noTerms}
+          />
+        </div>
+      </section>
+    </>
   );
 }
